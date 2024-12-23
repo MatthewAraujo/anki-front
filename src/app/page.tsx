7c88561618e20b7b93ad81292dd641f66030aac0
MatthewAraujo <matthewaraujo20@gmail.com>
2024-12-22 23:47:30 -0300

    opk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,32 +11,6 @@ export default function Home() {
       <Features />
       <PricingSectionCards />
 
-
-      < section id="how-it-works" className="py-16 bg-gray-50" >
-        <h2 className="text-2xl font-bold text-gray-800 text-center">
-          Como Usar
-        </h2>
-        <div className="mt-10 flex flex-col md:flex-row justify-between items-center gap-6 container">
-          <Card className="p-6 text-center">
-            <h3 className="font-semibold">1. Upload</h3>
-            <p className="mt-2 text-sm text-gray-600">
-              Adicione seu material de estudo.
-            </p>
-          </Card>
-          <Card className="p-6 text-center">
-            <h3 className="font-semibold">2. Configuração</h3>
-            <p className="mt-2 text-sm text-gray-600">
-              Escolha suas preferências.
-            </p>
-          </Card>
-          <Card className="p-6 text-center">
-            <h3 className="font-semibold">3. Geração</h3>
-            <p className="mt-2 text-sm text-gray-600">
-              Baixe seus cartões prontos!
-            </p>
-          </Card>
-        </div>
-      </section >
     </div >
   );
 }
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,16 +3,16 @@ import { Button } from "../ui/button";
 
 export function Hero() {
   return (
-    <section id="hero" className="text-center py-20 " >
-      <h1 className="text-4xl font-semibold">Learn More, Study Less</h1>
+    <section id="hero" className="text-center py-20">
+      <h1 className="text-4xl font-semibold">Aprenda Mais, Estude Menos</h1>
       <p className="mt-4 text-lg text-slate-500">
-        Create and sutyd flashcards in less time using <br />
-        our AI powered flashcards generator.
+        Transforme documentos em flashcards inteligentes <br />
+        com apenas dois passos simples. Nosso gerador, alimentado por IA, faz o resto!
       </p>
       <Button variant="default" className="mt-6 px-6 py-3 bg-black text-white rounded-full">
-        Get Started
+        Comece Agora
         <ArrowRight className="w-6 h-6 ml-2" />
       </Button>
-    </section >
-  )
+    </section>
+  );
 }
